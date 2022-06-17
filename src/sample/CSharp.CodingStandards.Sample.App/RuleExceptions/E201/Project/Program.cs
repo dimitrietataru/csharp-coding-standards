1d3a8441a7838919f3dd11b85687b0fd977887ad
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-06-17 18:40:06 +0300

    Define the 'E201' exception. Add code samples

---
+++ src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E201/Project/Program.cs
@@ -0,0 +1,11 @@
+namespace CSharp.CodingStandards.Sample.App.RuleExceptions.E201.Project;
+
+internal class Program
+{
+    public static void Main(object[] _)
+    {
+#pragma warning disable CA1303 // Do not pass literals as localized parameters
+        Console.WriteLine("Hello world!");
+#pragma warning restore CA1303 // Do not pass literals as localized parameters
+    }
+}
