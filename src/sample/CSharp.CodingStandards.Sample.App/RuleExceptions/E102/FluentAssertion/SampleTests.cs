756509949b9d20b7063d5ffd7477968fd8d970c8
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-06-17 23:12:16 +0300

    Define the 'E102' exception. Add code samples

---
+++ src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E102/FluentAssertion/SampleTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+
+namespace CSharp.CodingStandards.Sample.App.RuleExceptions.E102.FluentAssertion;
+
+public static class SampleTests
+{
+    private const string GivenValue = "123";
+    private const int ExpectedLength = 3;
+
+    ////[Fact]
+    internal static void GivenWhenThen()
+    {
+        // Arrange
+        string value;
+
+        // Act
+        value = GivenValue;
+
+        // Assert
+////#pragma warning disable IDE0058 // Expression value is never used
+        value.Should().NotBeNullOrEmpty();
+////#pragma warning restore IDE0058 // Expression value is never used
+
+        _ = value.Should().HaveLength(ExpectedLength);
+    }
+}
