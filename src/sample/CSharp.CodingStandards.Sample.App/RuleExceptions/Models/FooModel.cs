4310ba3f31e3d9712afb49877f8049377cf1754a
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-06-17 18:41:16 +0300

    Remove unused files

--- src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/Models/FooModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CSharp.CodingStandards.Sample.App.RuleExceptions.Models;
-
-////#pragma warning disable CA1819 // Properties should not return arrays
-public class FooModel
-{
-    public List<object> Navigations { get; set; } = new List<object>();
-    public byte[] RowVersion { get; set; } = Array.Empty<byte>();
-}
