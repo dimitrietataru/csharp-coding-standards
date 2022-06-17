f14c02f70fae47ff3efaa3eda0b4986e22dfb765
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-06-17 18:27:24 +0300

    Define the 'E002' exception. Add code samples

---
+++ src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E002/Models/Child/Subchild/SubchildModel.cs
@@ -0,0 +1,10 @@
+namespace CSharp.CodingStandards.Sample.App.RuleExceptions.E002.Models;
+
+////#pragma warning disable CA1002 // Do not expose generic lists
+////#pragma warning disable CA1819 // Properties should not return arrays
+////#pragma warning disable CA2227 // Collection properties should be read only
+public class SubchildModel
+{
+    public List<object> Items { get; set; } = new List<object>();
+    public object[] Objects { get; set; } = Array.Empty<object>();
+}
