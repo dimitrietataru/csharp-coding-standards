18cf0148ccee0d70c9403b7dca4e0df1840cae8c
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-06-17 18:33:26 +0300

    Define the 'E003' exception. Add code samples

---
+++ src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E003/Entities/Child/Subchild/SubchildEntity.cs
@@ -0,0 +1,10 @@
+namespace CSharp.CodingStandards.Sample.App.RuleExceptions.E003.Entities;
+
+////#pragma warning disable CA1002 // Do not expose generic lists
+////#pragma warning disable CA1819 // Properties should not return arrays
+////#pragma warning disable CA2227 // Collection properties should be read only
+public class SubchildEntity
+{
+    public List<object> Items { get; set; } = new List<object>();
+    public object[] Objects { get; set; } = Array.Empty<object>();
+}
