9fa65bb2482c585f0a9efbe3b3c16c7458a58fc6
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-06-17 23:12:33 +0300

    Define the 'E103' exception. Add code samples

---
+++ src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E103/EntityFramework/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace CSharp.CodingStandards.Sample.App.RuleExceptions.E103.EntityFramework;
+
+public sealed class User
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+}
---
+++ src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E103/EntityFramework/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CSharp.CodingStandards.Sample.App.RuleExceptions.E103.EntityFramework;
+
+public sealed class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+////#pragma warning disable IDE0058 // Expression value is never used
+        builder.Property(user => user.Id).IsRequired();
+////#pragma warning restore IDE0058 // Expression value is never used
+
+        // Alternative
+        _ = builder.Property(user => user.FullName).IsRequired().HasMaxLength(100);
+    }
+}
