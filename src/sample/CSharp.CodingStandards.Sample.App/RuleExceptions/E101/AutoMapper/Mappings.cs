11c00830baccc836fe69c5dbc3165df52d817aae
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-06-20 17:06:08 +0300

    Fix E101 rule matching pattern

---
+++ src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E101/AutoMapper/Mappings.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace CSharp.CodingStandards.Sample.App.RuleExceptions.E101.AutoMapper;
+
+public sealed class Mappings : Profile
+{
+    public Mappings()
+    {
+        ////#pragma warning disable IDE0058 // Expression value is never used
+        CreateMap<SourceDto, DestinationDto>()
+            .ForMember(
+                destination => destination.DestinationProperty,
+                options => options.MapFrom(source => source.SourceProperty));
+        ////#pragma warning restore IDE0058 // Expression value is never used
+    }
+}
