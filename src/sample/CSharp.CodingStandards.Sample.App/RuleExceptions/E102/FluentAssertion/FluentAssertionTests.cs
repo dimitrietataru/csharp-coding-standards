e0b5bf1b6c45739f342d3c4b7ab82cffe5573075
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-06-20 17:06:51 +0300

    Fix E102 rule matching pattern

--- src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E102/FluentAssertion/FluentAssertionTests.cs
+++ src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E102/FluentAssertion/FluentAssertionTests.cs
@@ -2,7 +2,7 @@
 
 namespace CSharp.CodingStandards.Sample.App.RuleExceptions.E102.FluentAssertion;
 
-public static class SampleTests
+public static class FluentAssertionTests
 {
     private const string GivenValue = "123";
     private const int ExpectedLength = 3;
