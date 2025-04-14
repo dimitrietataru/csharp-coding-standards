437ee073b10745d0a5a1af9d2768ada2ba19f09b
Dimitrie Tataru <dimitriettr@gmail.com>
2025-04-14 06:48:16 +0300

    Fix CA 1852 warnings

--- src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E003/Entities/Child/ChildEntity.cs
+++ src/sample/CSharp.CodingStandards.Sample.App/RuleExceptions/E003/Entities/Child/ChildEntity.cs
@@ -3,7 +3,7 @@ namespace CSharp.CodingStandards.Sample.App.RuleExceptions.E003.Entities;
 ////#pragma warning disable CA1002 // Do not expose generic lists
 ////#pragma warning disable CA1819 // Properties should not return arrays
 ////#pragma warning disable CA2227 // Collection properties should be read only
-internal class ChildEntity
+internal sealed class ChildEntity
 {
     public List<object> Items { get; set; } = [];
     public object[] Objects { get; set; } = [];
